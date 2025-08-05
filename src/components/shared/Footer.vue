09034766f86099064765416662648c30fed143ae
trinidj <jm.trinidad199@gmail.com>
2025-08-05 17:49:07 -0400

    feat: add basic Header and Footer component structure

---
+++ src/components/shared/Footer.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+  <footer id="footer-section">
+    Footer Section
+  </footer>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/shared/Header.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+</script>
+
+<template>
+  <header id="header-section">
+    <nav>
+      Header Section
+    </nav>
+  </header>
+</template>
+
+<style scoped>
+
+</style>
