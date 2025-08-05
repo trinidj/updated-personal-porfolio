443e4db56dfbc34758bb0f98d941ae2e2b91d22d
trinidj <jm.trinidad199@gmail.com>
2025-08-05 17:50:58 -0400

    feat: integrate portfolio components into main App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,18 @@
 <script setup>
-
+  import Layout from '@/components/layout/Layout.vue';
+  import HeroSection from '@/components/HeroSection.vue';
+  import AboutSection from '@/components/AboutSection.vue';
+  import ProjectsSection from '@/components/ProjectsSection.vue';
+  import ContactsForm from '@/components/ContactsForm.vue';
 </script>
 
 <template>
-  
+  <Layout>
+    <HeroSection />
+    <AboutSection />
+    <ProjectsSection />
+    <ContactsForm />
+  </Layout>
 </template>
 
 <style scoped>
