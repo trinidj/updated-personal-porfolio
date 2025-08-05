5ed8aece1d0a74d33d45c5c62cfdd1f47687cbb8
trinidj <jm.trinidad199@gmail.com>
2025-08-05 17:42:24 -0400

    first commit

---
+++ src/components/layout/Layout.vue
@@ -0,0 +1,18 @@
+<script setup>
+  import Header from '@/components/shared/Header.vue';
+  import Footer from '@/components/shared/Footer.vue';
+</script>
+
+<template>
+  <body>
+    <Header />
+    <main>
+      <slot />
+    </main>
+    <Footer />
+  </body>
+</template>
+
+<style scoped>
+
+</style>
