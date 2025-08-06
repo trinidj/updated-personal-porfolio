d878326db0afceacd3f4cb1ec6c39518eb9398a6
trinidj <jm.trinidad199@gmail.com>
2025-08-06 16:57:35 -0400

    feat: enhance Header component with styled navigation and animations

--- src/components/shared/Header.vue
+++ src/components/shared/Header.vue
@@ -1,6 +1,17 @@
 <script setup>
   import Button from '@/components/ui/Button.vue';
   import { Mail } from 'lucide-vue-next';
+  import { animate } from 'animejs';
+
+  const navItems = ['Project', 'About', 'Experience', 'Skills'];
+
+  const slideIn = (event) => {
+    animate(event.target, {});
+  };
+
+  const slideOut = (event) => {
+    animate(event.target, {});
+  }
 </script>
 
 <template>
@@ -12,16 +23,23 @@
 
       <div class="navigation-container">
         <ul class="list">
-          <li class="list-item">Project</li>
-          <li class="list-item">About</li>
-          <li class="list-item">Experience</li>
-          <li class="list-item">Skills</li>
+          <li 
+            v-for="item in navItems" 
+            :key="item" 
+            class="list-item" 
+            @mouseover="slideIn" 
+            @mouseout="slideOut"
+          >
+            <a href="">{{ item }}</a>
+          </li>
         </ul>
       </div>
 
       <div class="socials-container">
-        <Button>
-          <Mail />
+        <Button class="contact-button">
+          <Mail 
+            :size="16"
+          />
           <span>Contact Me</span>
         </Button>
       </div>
@@ -35,5 +53,76 @@
     flex-direction: row;
     align-items: center;
     justify-content: space-between;
+    background: hsl(from var(--color-background-primary) h s calc(l - 5) / 0.2);
+    padding: var(--spacing-lg) var(--spacing-5xl);
+    border-bottom: 1px solid hsl(from var(--color-background-primary) h s calc(l + 10));
+  }
+
+  .site-title {
+    font-size: var(--text-2xl);
+    font-weight: 700;
+
+    background-image: linear-gradient(to right, #99E1D9, #F0F7F4);
+
+    background-clip: text;
+    -webkit-background-clip: text; 
+
+    color: transparent;
+    -webkit-text-fill-color: transparent; 
+  }
+
+  .list {
+    display: flex;
+    flex-direction: row;
+    list-style-type: none;
+    gap: var(--spacing-xl);
+    cursor: pointer;
+    position: relative;
+  }
+
+  .contact-button {
+    background: hsl(from var(--color-background-primary) h s calc(l - 10) / 0.2);
+    border: 1px solid hsl(from var(--color-background-primary) h s calc(l + 10));
+    color: white;
+    transition: all 0.2s ease;
+  }
+
+  .contact-button:hover {
+    cursor: pointer;
+    background-color: hsl(from var(--color-accent-primary) h s l / 0.2);
+    border: 1px solid var(--color-accent-secondary);
+  }
+
+  .list-item {
+    position: relative;
+    font-weight: 500;
+    color: hsl(from var(--color-text-primary) h s calc(l + 5) / 0.5);
+  }
+  
+  .list-item a {
+    text-decoration: none;
+  }
+
+  .list-item a:link, a:visited {
+    color: hsl(from var(--color-text-primary) h s calc(l + 5) / 0.5);
+  }
+  
+  .list-item a:hover {
+    color: var(--color-accent-primary);
+  }
+
+  .list-item::after {
+    content: '';
+    position: absolute;
+    left: 0;
+    width: 0; 
+    height: 2px; 
+    transition: width 0.3s ease; 
+    background-color: var(--color-accent-primary);
+    bottom: -5px;
+  }
+
+  .list-item:hover::after {
+    width: 100%; 
   }
 </style>
