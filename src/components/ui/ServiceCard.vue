6284ef4c7d627f3c689a7ee714c0aa10dd761f9d
trinidj <jm.trinidad199@gmail.com>
2025-08-07 17:43:04 -0400

    feat: enhance ServiceCard component with improved props and styling

--- src/components/ui/ServiceCard.vue
+++ src/components/ui/ServiceCard.vue
@@ -4,9 +4,13 @@
       type: String,
       default: '',
     },
-    desc: {
+    description: {
       type: String,
       default: '',
+    },
+    icon: {
+      type: Object,
+      default: null,
     },
   });
 </script>
@@ -14,17 +18,23 @@
 <template>
   <div class="service-card">
     <div class="icon-container">
-      <slot />
+      <div class="icon">
+        <component 
+          v-if="props.icon" 
+          :is="props.icon" 
+        />
+        <slot v-else />
+      </div>
     </div>
 
     <div class="content-container">
       <div class="service-content">
         <div class="service-header">
-          {{ props.title }}
+          <h4>{{ props.title }}</h4>
         </div>
 
         <div class="service-description">
-          {{ props.desc }}
+          <p>{{ props.description }}</p>
         </div>
       </div>
     </div>
@@ -35,7 +45,33 @@
   .service-card {
     display: flex;
     flex-direction: row;
-    padding: var(--spacing-xl);
+    /* align-items: center; */
+    gap: var(--spacing-base);
+    padding: var(--spacing-lg);
     background: hsl(from var(--color-background-primary) h s calc(l - 3) / 0.5);
+    border: 1px solid hsl(from var(--color-accent-primary) h s calc(l - 10) / 0.5);
+    border-radius: 15px;
+  }
+
+  .icon {
+    display: flex;
+    color: #99E1D9;
+    border-radius: 10px;
+    background: hsl(from var(--color-accent-secondary) h s l / 0.5);
+    padding: var(--spacing-sm);
+  }
+
+  .service-content {
+    display: flex;
+    flex-direction: column;
+    gap: var(--spacing-sm);
+  }
+
+  .service-header h4 {
+    font-size: var(--text-xl);
+  }
+
+  .service-description p {
+    color: hsl(from var(--color-text-primary) h s l / 0.7);
   }
 </style>
