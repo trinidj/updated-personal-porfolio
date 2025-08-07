46e4847b30f6283d7a118008e0cbeedca726af29
trinidj <jm.trinidad199@gmail.com>
2025-08-07 13:08:30 -0400

    feat: add ServiceCard component for service section display

---
+++ src/components/ui/ServiceCard.vue
@@ -0,0 +1,25 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="service-card">
+    <div class="icon-container">
+      <slot />
+    </div>
+
+    <div class="service-content">
+      <div class="service-header">
+        <slot />
+      </div>
+
+      <div class="service-description">
+        <slot />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  
+</style>
