c7e4b0a8cd81a818f352c267aa8ff7b0fc18ed8f
trinidj <jm.trinidad199@gmail.com>
2025-08-07 14:31:45 -0400

    feat: enhance ServiceCard component with props and styling

--- src/components/ui/ServiceCard.vue
+++ src/components/ui/ServiceCard.vue
@@ -1,5 +1,14 @@
 <script setup>
-
+  const props = defineProps({
+    title: {
+      type: String,
+      default: '',
+    },
+    desc: {
+      type: String,
+      default: '',
+    },
+  });
 </script>
 
 <template>
@@ -8,18 +17,25 @@
       <slot />
     </div>
 
-    <div class="service-content">
-      <div class="service-header">
-        <slot />
-      </div>
+    <div class="content-container">
+      <div class="service-content">
+        <div class="service-header">
+          {{ props.title }}
+        </div>
 
-      <div class="service-description">
-        <slot />
+        <div class="service-description">
+          {{ props.desc }}
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <style scoped>
-  
+  .service-card {
+    display: flex;
+    flex-direction: row;
+    padding: var(--spacing-xl);
+    background: hsl(from var(--color-background-primary) h s calc(l - 3) / 0.5);
+  }
 </style>
